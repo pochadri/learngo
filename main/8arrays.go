9021dc126b99735c5ba4bb0da20aa7aa8509d57b
pochadri <pochadri@gmail.com>
2018-02-18 20:49:26 +0530

    arrays added

---
+++ main/8arrays.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// this is array of type [3]int, size is part of the array definition
+	// all elements initialized to 0
+	// array indexes start from 0 :) its obvious, no so obvious for people from Lua Pascal Fortran :-D
+	var a [3]int
+
+	// set value
+	a[1] = 10;
+
+	fmt.Println("set:",a)
+	fmt.Println("element:",a[2])
+
+	// initialize and define in one statement
+	b:=[5]int{10,20,30,40,50}
+
+	fmt.Println(b)
+
+	// two dimenational array
+
+	var twoD [2][3]int;
+
+	for i:=0;i<2;i++ {
+		for j:=0;j<3;j++ {
+			twoD[i][j] = i+j;
+			}
+	}
+
+	fmt.Println(twoD)
+
+
+}
